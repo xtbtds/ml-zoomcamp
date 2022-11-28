8c070ba2edc5eb096ee73887d1cabe2ac47bd435
xtbtds <51954806+xtbtds@users.noreply.github.com>
2022-11-28 14:01:06 +0100

    Add files via upload

---
+++ homework9/homework9.py
@@ -0,0 +1,54 @@
+
+#!wget https://github.com/SVizor42/ML_Zoomcamp/releases/download/dino-dragon-model/dino_dragon_10_0.899.h5 -O hw_model.h5
+import tensorflow as tf
+from tensorflow import keras
+import tensorflow.lite as tflite
+from io import BytesIO
+from urllib import request
+from PIL import Image
+import numpy as np
+
+#_____load and save model_______
+model = keras.models.load_model('hw_model.h5')
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+with open('clothing-model.tflite', 'wb') as f_out:
+    f_out.write(tflite_model)
+
+
+#_____input and output indexes_____
+interpreter = tflite.Interpreter(model_path='clothing-model.tflite')
+interpreter.allocate_tensors()
+input_index = interpreter.get_input_details()[0]['index']
+output_index = interpreter.get_output_details()[0]['index']
+
+
+#____prepare image______
+def download_image(url):
+    with request.urlopen(url) as resp:
+        buffer = resp.read()
+    stream = BytesIO(buffer)
+    img = Image.open(stream)
+    return img
+def prepare_image(img, target_size):
+    if img.mode != 'RGB':
+        img = img.convert('RGB')
+    img = img.resize(target_size, Image.NEAREST)
+    return img
+def prepare_input(x):
+    return x * (1. / 255)
+
+
+#______predict______
+url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Smaug_par_David_Demaret.jpg/1280px-Smaug_par_David_Demaret.jpg'
+def predict(url):
+    img = download_image(url)
+    img = prepare_image(img, target_size=(150, 150))
+    x = np.array(img, dtype = 'float32')
+    X = np.array([x])
+    X = prepare_input(X)
+
+    interpreter.set_tensor(input_index, X)
+    interpreter.invoke()
+    preds = interpreter.get_tensor(output_index)
+    return float(preds[0,0])
